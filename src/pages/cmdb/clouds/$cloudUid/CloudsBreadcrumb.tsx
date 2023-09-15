80f26d130412a1dd7a708ab3e1f1a9adddbda9b6
mosl <mosl@appexnetworks.com>
2023-09-15 14:14:47 +0800

    feat: add cloud breadcrumb margin

--- src/pages/cmdb/clouds/$cloudUid/CloudsBreadcrumb.tsx
+++ src/pages/cmdb/clouds/$cloudUid/CloudsBreadcrumb.tsx
@@ -6,18 +6,20 @@ export default function CloudsBreadcrumb() {
   const { cloud } = useCloud();
 
   return (
-    <Breadcrumb
-      items={[
-        {
-          title: '资源管理',
-        },
-        {
-          title: <Link to="/cmdb/clouds">云商管理</Link>,
-        },
-        {
-          title: cloud?.CloudName,
-        },
-      ]}
-    />
+    <div className="ml-4">
+      <Breadcrumb
+        items={[
+          {
+            title: '资源管理',
+          },
+          {
+            title: <Link to="/cmdb/clouds">云商管理</Link>,
+          },
+          {
+            title: cloud?.CloudName,
+          },
+        ]}
+      />
+    </div>
   );
 }
