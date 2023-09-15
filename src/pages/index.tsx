5850ded090e2dbea96c2f1896aa4883747724f51
mosl <mosl@appexnetworks.com>
2023-09-15 17:55:35 +0800

    feat: select table row fix cell color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,5 @@
 import PageContainer from '@/components/ui/PageContainer';
-import { ColorPicker } from 'antd';
 
 export default function Home() {
-  return (
-    <PageContainer>
-      <ColorPicker size="large" />
-    </PageContainer>
-  );
+  return <PageContainer>Homes</PageContainer>;
 }
