14f30488cc7f7d88588992a06edb938c9189a708
mosl <mosl@appexnetworks.com>
2023-12-28 14:53:49 +0800

    feat: 取消子任务执行步骤重试按钮权限

--- src/pages/jobs/tasks/_components/sub-task-phase-info.tsx
+++ src/pages/jobs/tasks/_components/sub-task-phase-info.tsx
@@ -203,32 +203,30 @@ export default function SubTaskPhaseInfo({
               column={2}
               extra={
                 <div className="flex gap-x-1">
-                  {(phase.retry || user?.username === "lightops") && (
-                    <Button
-                      disabled={!access.phaseRunApiOpsByPhasesid}
-                      onClick={() => {
-                        modal.confirm({
-                          title: `确定要重试 ${phase.name} ？`,
-                          icon: <ExclamationCircleFilled />,
-                          onOk: async () => {
-                            await phaseRunApiOpsByPhasesid(
-                              {
-                                id: String(phase.id),
-                              },
-                              {},
-                            )
-                            message.success("已重试")
-                            refetch()
-                            queryClient.invalidateQueries({
-                              queryKey: ["sub-tasks"],
-                            })
-                          },
-                        })
-                      }}
-                    >
-                      重试
-                    </Button>
-                  )}
+                  <Button
+                    disabled={!access.phaseRunApiOpsByPhasesid}
+                    onClick={() => {
+                      modal.confirm({
+                        title: `确定要重试 ${phase.name} ？`,
+                        icon: <ExclamationCircleFilled />,
+                        onOk: async () => {
+                          await phaseRunApiOpsByPhasesid(
+                            {
+                              id: String(phase.id),
+                            },
+                            {},
+                          )
+                          message.success("已重试")
+                          refetch()
+                          queryClient.invalidateQueries({
+                            queryKey: ["sub-tasks"],
+                          })
+                        },
+                      })
+                    }}
+                  >
+                    重试
+                  </Button>
 
                   {phase.status === "Confirm" && (
                     <Button
