4751e4eac9e81ee9397ff3ea05069cc2e8f97eef
mosl <ranye1004@gmail.com>
2024-04-15 16:36:15 +0800

    refactor: 删除console.log

--- src/pages/ci/deploy/_components/deploy-modal-form.tsx
+++ src/pages/ci/deploy/_components/deploy-modal-form.tsx
@@ -180,7 +180,6 @@ export default function DeployModalForm({
         onFinish?.()
         return true
       }}
-      onValuesChange={console.log}
       initialValues={
         {
           product: "Orch",
