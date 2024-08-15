a3d2a3ffb1576a039b27964d8682488c3b603de1
mosl <ranye1004@gmail.com>
2024-08-15 14:54:57 +0800

    feat: 任务详情支持自动刷新

--- src/pages/ibex/tasks/$id/index.tsx
+++ src/pages/ibex/tasks/$id/index.tsx
@@ -1,7 +1,9 @@
+import { useLocalStorageState } from "@/lib/hooks/use-local-storage-state"
 import { taskReadOneApiIbexByTasksid } from "@/services/ibex/tpl"
+import { RollbackOutlined, SyncOutlined } from "@ant-design/icons"
 import { useQuery } from "@tanstack/react-query"
-import { useParams } from "@umijs/max"
-import { Card, Divider, Typography } from "antd"
+import { history, useParams } from "@umijs/max"
+import { Button, Card, Divider, Select, Space, Tooltip, Typography } from "antd"
 import ScriptInput from "../../_components/script-input"
 import HostTable from "../_components/host-table"
 import TaskDetails from "../_components/task-details"
@@ -9,15 +11,67 @@ import TaskDetails from "../_components/task-details"
 export default function TaskDetailPage() {
   const { id } = useParams()
 
-  const { data } = useQuery({
+  const [refetchInterval, setRefetchInterval] = useLocalStorageState<
+    false | number
+  >("tpl-task-fetch-interval", false)
+
+  const { data, refetch } = useQuery({
     queryKey: ["ibex-task", id],
     queryFn: () =>
       taskReadOneApiIbexByTasksid({ id: id! }).then((res) => res.data),
-    refetchInterval: 3000,
+    refetchInterval,
   })
 
   return (
-    <Card title={data?.meta?.title} classNames={{ body: "!pt-0" }}>
+    <Card
+      title={
+        <div className="flex items-center gap-2">
+          <Button
+            type="text"
+            icon={<RollbackOutlined />}
+            onClick={() => history.replace("/ibex/tasks")}
+          />
+          {data?.meta?.title}
+          <Space.Compact className="ml-auto">
+            <Tooltip title="手动刷新">
+              <Button icon={<SyncOutlined />} onClick={() => refetch()} />
+            </Tooltip>
+            <Select
+              value={refetchInterval}
+              style={{ width: 56 }}
+              onChange={(value) => setRefetchInterval(value)}
+              options={[
+                {
+                  label: "off",
+                  value: false,
+                },
+                {
+                  label: "3s",
+                  value: 3 * 1000,
+                },
+                {
+                  label: "5s",
+                  value: 5 * 1000,
+                },
+                {
+                  label: "10s",
+                  value: 10 * 1000,
+                },
+                {
+                  label: "30s",
+                  value: 30 * 1000,
+                },
+                {
+                  label: "60s",
+                  value: 60 * 1000,
+                },
+              ]}
+            />
+          </Space.Compact>
+        </div>
+      }
+      classNames={{ body: "!pt-0" }}
+    >
       {data && (
         <div className="mt-6">
           {[
