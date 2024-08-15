4037162c2062db3ae421e8acffc76f13a0dec043
mosl <ranye1004@gmail.com>
2024-08-15 14:47:26 +0800

    feat: 脚本可复制

--- src/pages/ibex/_components/script-input.tsx
+++ src/pages/ibex/_components/script-input.tsx
@@ -1,6 +1,7 @@
 import { loadLanguage } from "@uiw/codemirror-extensions-langs"
 import { vscodeDark } from "@uiw/codemirror-theme-vscode"
 import CodeMirror, { EditorView } from "@uiw/react-codemirror"
+import { Typography } from "antd"
 
 export interface ScriptInputProps {
   value?: string
@@ -16,19 +17,25 @@ export default function ScriptInput({
   inputRef,
 }: ScriptInputProps) {
   return (
-    <CodeMirror
-      value={value}
-      onChange={onChange}
-      onCreateEditor={(view) => {
-        if (inputRef) {
-          inputRef.current = view
-        }
-      }}
-      minHeight="100px"
-      maxHeight="500px"
-      theme={vscodeDark}
-      editable={!disabled}
-      extensions={[loadLanguage("shell")!].filter(Boolean)}
-    />
+    <div className="relative">
+      <CodeMirror
+        value={value}
+        onChange={onChange}
+        onCreateEditor={(view) => {
+          if (inputRef) {
+            inputRef.current = view
+          }
+        }}
+        minHeight="100px"
+        maxHeight="500px"
+        theme={vscodeDark}
+        editable={!disabled}
+        extensions={[loadLanguage("shell")!].filter(Boolean)}
+      />
+      <Typography.Text
+        copyable={{ text: value }}
+        className="absolute right-2 top-2"
+      />
+    </div>
   )
 }
